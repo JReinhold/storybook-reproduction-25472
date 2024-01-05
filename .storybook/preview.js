3641a7b499a91232ed7ca3cdb7fa7ebc737c5757
Jeppe Reinhold <jeppe@reinhold.is>
2024-01-05 10:36:08 +0100

    add chromatic workaround

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import results from '../.jest-test-results.json';
+import { withTests } from '@storybook/addon-jest';
+import isChromatic from "chromatic/isChromatic";
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -8,6 +12,10 @@ const preview = {
       },
     },
   },
+  decorators: [
+    // don't add withTests when Chromatic is snapshotting because they'll cause Safari to fail
+    !isChromatic() ? withTests({ results }) : (story) => story()
+  ],
 };
 
 export default preview;
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,7 +1,5 @@
 import { fn } from '@storybook/test';
 import { Button } from './Button';
-import results from '../../.jest-test-results.json';
-import { withTests } from '@storybook/addon-jest';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
@@ -19,8 +17,6 @@ export default {
   },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
-  decorators: [withTests({ results })],
-
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
